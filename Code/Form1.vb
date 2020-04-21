8f05a3086f6b64357d420871b4ada280a765697b
MakeItSimple <63766598+MakeItSimple6@users.noreply.github.com>
2020-04-21 13:13:04 +0530

    Add files via upload

---
+++ Code/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub AddNewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddNewToolStripMenuItem.Click
+        'Add'
+        add_student.Show()
+    End Sub
+End Class
